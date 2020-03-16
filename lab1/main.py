85e9a178a096cad9a460014e270f83283935d371
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-03-16 12:28:45 +0100

    create a jupiter notebook; load boston dataset; print df.head(); histogram of two columns: AGE and TAX

--- lab1/main.py
+++ lab1/main.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
-import matplotlib
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_boston
 
+boston_data = load_boston()
+boston_df = pd.DataFrame(boston_data['data'], columns=boston_data['feature_names'])
+boston_df['target'] = boston_data['target']
+print(boston_df.head())
 
-X, y = load_boston(return_X_y=True)
+pd.DataFrame.hist(boston_df,['AGE','TAX'])
+plt.show()
