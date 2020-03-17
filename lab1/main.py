17382dee64812c09d9ea6e3ec56d5803cebd734e
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-03-17 10:53:59 +0100

    add linear reggression and update jupyter notebook

--- lab1/main.py
+++ lab1/main.py
@@ -2,11 +2,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_boston
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 boston_data = load_boston()
 boston_df = pd.DataFrame(boston_data['data'], columns=boston_data['feature_names'])
-boston_df['target'] = boston_data['target']
+boston_df['PRICE'] = boston_data['target']
 print(boston_df.head())
-
-pd.DataFrame.hist(boston_df,['AGE','TAX'])
+plt.figure(num=1,figsize=(20, 5))
+features = ['LSTAT', 'RM']
+reg = LinearRegression()
+price = np.array(boston_df['PRICE']).reshape(-1,1)
+for i, col in enumerate(features):
+    plt.subplot(1, len(features) , i+1)
+    plt.scatter(boston_df[col], boston_df['PRICE'], marker='o')
+    plt.xlabel(col)
+    plt.ylabel('PRICE')
+    column = np.array(boston_df[col]).reshape(-1,1)
+    reg.fit(column,price)
+    predicted_price = reg.predict(column)
+    plt.plot(column, predicted_price, 'r')
+pd.DataFrame.hist(boston_df,['LSTAT','RM'])
 plt.show()
